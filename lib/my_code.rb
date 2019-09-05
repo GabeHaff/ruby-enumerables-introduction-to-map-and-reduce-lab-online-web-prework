61bc1c200b8643777db590b399f52a753cc68221
Gabriel Hafferty <gabe.hafferty.business@gmail.com>
2019-09-05 12:13:33 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,5 +1,12 @@
 # My Code here....
-def map_to_negativize()
+def map_to_negativize(source_array)
 
+   i = 0
+  new_array = []
+
+   while i < source_array.length do
+
+   new_array << source_array[i] * -1
+  i += 1
 return map_to_negativize
 end
