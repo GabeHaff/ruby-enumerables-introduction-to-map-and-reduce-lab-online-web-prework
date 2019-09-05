8afbc317e11035a9a0636f2283ad30c318eb93a5
Gabriel Hafferty <gabe.hafferty.business@gmail.com>
2019-09-05 13:13:31 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -50,19 +50,19 @@ end
 total 
 end 
 
- def reduce_to_all_true(source_array)
+ def reduce_to_all_true(array)
   counter = 0
   while counter < array.size do 
-  return false if !source_array[counter] 
+  return false if !array[counter] 
   counter +=1
 end
 return true 
 end 
 
-def reduce_to_any_true(source_array)
+def reduce_to_any_true(array)
   counter = 0 
   while counter < array.size do 
-    return true if source_array[counter]
+    return true if array[counter]
     counter+=1 
   end 
   return false 
